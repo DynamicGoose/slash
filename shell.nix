956113b671c127828078b489e9c47f1813d66718
DynamicGoose <gezaahs@gmail.com>
2023-09-30 10:26:53 +0200

    update

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
-  name = "iced-env";
+  name = "slint-env";
   buildInputs = [
-    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr libGL freetype pkg-config freetype.dev expat wayland libxkbcommon
+    xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr libGL freetype pkg-config freetype.dev expat wayland libxkbcommon fontconfig
   ];
 
   # WINIT_UNIX_BACKEND=wayland/x11
