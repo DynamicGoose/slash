646cbbe843939b38ad009cac4e66e7d95c6f9f46
DynamicGoose <gezaahs@gmail.com>
2023-10-02 16:53:32 +0200

    fonts

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let config =slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
+    let config = slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
     
     slint_build::compile_with_config("ui/layout.slint", config).unwrap();
 }
