ac8eecfdb0f2db61953d22fd0b2f77b789e4b2a8
DynamicGoose <gezaahs@gmail.com>
2023-10-05 23:13:59 +0200

    implement ui logic basis

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use slint::{ComponentHandle, VecModel, ModelRc};
+
 slint::include_modules!();
 
 fn main() {
-    MainWindow::new().unwrap().run().unwrap();
+    let handle = MainWindow::new().unwrap();
+
+    let search_handle = handle.as_weak();
+    handle.global::<Logic>().on_search(move || {
+        let search_text = search_handle.unwrap().global::<Logic>().get_searchText();
+        let search_handle = search_handle.clone();
+        std::thread::spawn(move || {
+            slint::invoke_from_event_loop(move || search_handle.unwrap().global::<Logic>().set_mainViewData(ModelRc::new(VecModel::from(vec!["searched for:".into(), search_text.into()])))).unwrap();
+        });
+    });
+
+    handle.run().unwrap();
 }
